5c5c98725cfbe2c0804c3bd6835e01a0fce08e90
Karol Moroz <k.j.moroz@gmail.com>
2021-02-10 14:11:36 +0800

    Add documentation for ECPayInvoice and Payload modules

--- lib/ecpay_invoice.ex
+++ lib/ecpay_invoice.ex
@@ -1,18 +1,38 @@
-defmodule EcpayInvoice do
+defmodule ECPayInvoice do
   @moduledoc """
-  Documentation for `EcpayInvoice`.
+  Client library for the ECPay Invoice API provided by Green World
+  FinTech Service Co. (Taipei, Taiwan, Chinese name: 綠界科技股份有限公司).
+
+  This library is compatible with API revision 3.0.0, which talks to
+  the server using JSON payloads and uses AES encryption for verification.
+  This library depends on HTTPoison and Jason.
+
+  In order to use this library in production, you need to configure your API
+  credentials in your configuration files (`config/prod.secret.exs`,
+  `config/runtime.exs` etc.) by setting the `profiles` and `default_profile`
+  keys for the `:ecpay_invoice` OTP application:
+
+  ```elixir
+  use Mix.Config
+
+  config :ecpay_invoice, :profiles, %{
+    production: %{
+      development: false,
+      merchant_id: "1420420",
+      hash_iv: "CHANGE_ME",
+      hash_key: "CHANGE_ME"
+    }
+  }
+
+  config :ecpay_invoice, :default_profile, :production
+  ```
+
+  If you have multiple ECPay accounts, for instance, if your application
+  serves multiple branches, you can set multiple configuration profiles
+  and pass the profile name when performing the actual requests.
+  Please note that the profile name can be either a string or an atom
+  when performing requests (which is useful if you need to determine the
+  profile name dynamically) but needs to be passed as atom in your runtime
+  configuration.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> EcpayInvoice.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- lib/ecpay_invoice/payload.ex
+++ lib/ecpay_invoice/payload.ex
@@ -2,6 +2,23 @@ defmodule ECPayInvoice.Payload do
   alias ECPayInvoice.Config
   alias ECPayInvoice.Crypto
 
+  @moduledoc """
+  This module contains functions related to encoding request payloads
+  to the format expected by the ECPay API.
+  """
+
+  @doc """
+  With a given `payload` as map and a `profile` name as string or atom,
+  encodes the given payload to the JSON format expected by the ECPay
+  Invoice API, putting the respective fields of the request, such as
+  `Revision` key, `MerchantID`, and `Timestamp`, in the correct parts
+  of the JSON object.
+
+  Serializes the original `payload` as JSON, URL-encodes it, encrypts
+  it using the AES cipher as required by the API, and serializes the
+  resulting binary to Base64. For more details regarding this step, see
+  `ECPayInvoice.Crypto.encrypt_payload/2`.
+  """
   @spec encode(data :: map(), profile :: atom() | binary()) :: binary()
   def encode(data, profile) when is_map(data) do
     Jason.encode!(%{
